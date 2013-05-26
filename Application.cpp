d71bafa883db2df455961985beaa3a06a1311ce5
Bartosz Taudul <wolf.pld@gmail.com>
2013-05-26 16:54:43 +0200

    Print debug messages to stderr.

--- Application.cpp
+++ Application.cpp
@@ -1,4 +1,19 @@
+#include <stdio.h>
+#include <memory>
+
+#include "Debug.hpp"
+
+struct DebugCallback_t : public DebugLog::Callback
+{
+    void OnDebugMessage( const char* msg ) override
+    {
+        fprintf( stderr, "%s\n", msg );
+    }
+} DebugCallback;
+
 int main( int argc, char** argv )
 {
+    DebugLog::AddCallback( &DebugCallback );
+
     return 0;
 }
