27c20cd9fb4215a538e81124f5f13ffd18154be3
Bartosz Taudul <wolf.pld@gmail.com>
2013-05-31 19:33:02 +0200

    Add turbo mode for alpha channel compression.

--- BlockData.cpp
+++ BlockData.cpp
@@ -27,6 +27,16 @@ static v3b Average( const uint8* data )
     return v3b( r / 8, g / 8, b / 8 );
 }
 
+static uint8 Average1( const uint8* data )
+{
+    uint32 a = 0;
+    for( int i=0; i<8; i++ )
+    {
+        a += *data++;
+    }
+    return a / 8;
+}
+
 static Color::Lab Average( const Color::Lab* data )
 {
     Color::Lab ret;
@@ -67,6 +77,16 @@ static uint CalcError( const uint8* data, const v3b& average )
         uint32 g = *data++;
         uint32 r = *data++;
         err += sq( r - average.x ) + sq( g - average.y ) + sq( b - average.z );
+    }
+    return err;
+}
+
+static uint CalcError( const uint8* data, uint8 average )
+{
+    uint err = 0;
+    for( int i=0; i<8; i++ )
+    {
+        err += sq( *data++ - average );
     }
     return err;
 }
@@ -388,6 +408,28 @@ static void ProcessAverages( v3b* a )
     }
 }
 
+static void ProcessAverages( uint8* a )
+{
+    for( int i=0; i<2; i++ )
+    {
+        int32 c1 = a[i*2+1] >> 3;
+        int32 c2 = a[i*2] >> 3;
+
+        int32 diff = c2 - c1;
+        diff = std::min( std::max( diff, -4 ), 3 );
+
+        int32 co = c1 + diff;
+
+        a[5+i*2] = ( c1 << 3 ) | ( c1 >> 2 );
+        a[4+i*2] = ( co << 3 ) | ( co >> 2 );
+    }
+    for( int i=0; i<4; i++ )
+    {
+        a[i] &= 0xF0;
+        a[i] |= a[i] >> 4;
+    }
+}
+
 static void EncodeAverages( uint64& d, const v3b* a, size_t idx )
 {
     d |= idx;
@@ -407,6 +449,31 @@ static void EncodeAverages( uint64& d, const v3b* a, size_t idx )
         {
             d |= uint64( a[base+1][i] & 0xF8 ) << ( i*8 + 8 );
             int8 c = ( ( a[base+0][i] & 0xF8 ) - ( a[base+1][i] & 0xF8 ) ) >> 3;
+            c &= ~0xF8;
+            d |= ((uint64)c) << ( i*8 + 8 );
+        }
+    }
+}
+
+static void EncodeAverages( uint64& d, const uint8* a, size_t idx )
+{
+    d |= idx;
+    size_t base = idx << 1;
+
+    if( ( idx & 0x2 ) == 0 )
+    {
+        for( int i=0; i<3; i++ )
+        {
+            d |= uint64( a[base+0] & 0xF0 ) << ( i*8 + 4 );
+            d |= uint64( a[base+1] & 0xF0 ) << ( i*8 + 8 );
+        }
+    }
+    else
+    {
+        for( int i=0; i<3; i++ )
+        {
+            d |= uint64( a[base+1] & 0xF8 ) << ( i*8 + 8 );
+            int8 c = ( ( a[base+0] & 0xF8 ) - ( a[base+1] & 0xF8 ) ) >> 3;
             c &= ~0xF8;
             d |= ((uint64)c) << ( i*8 + 8 );
         }
@@ -638,7 +705,104 @@ static uint64 ProcessRGB( const uint8* src )
 
 static uint64 ProcessAlpha( const uint8* src )
 {
-    return 0;
+    uint64 d = 0;
+
+    {
+        bool solid = true;
+        const uint8* ptr = src + 1;
+        for( int i=1; i<16; i++ )
+        {
+            if( *src != *ptr++ )
+            {
+                solid = false;
+                break;
+            }
+        }
+        if( solid )
+        {
+            uint c = *src & 0xF8;
+            d |= 0x2 | ( c << 24 ) | ( c << 16 ) | ( c << 8 );
+            return d;
+        }
+    }
+
+    uint8 b[4][8];
+
+    memcpy( b[1], src, 8 );
+    memcpy( b[0], src+8, 8 );
+
+    for( int i=0; i<4; i++ )
+    {
+        *(b[3]+i*2) = *(src+i*4);
+        *(b[2]+i*2) = *(src+i*4+3);
+    }
+
+    uint8 a[8];
+    for( int i=0; i<4; i++ )
+    {
+        a[i] = Average1( b[i] );
+    }
+    ProcessAverages( a );
+
+    uint err[4] = {};
+    for( int i=0; i<4; i++ )
+    {
+        err[i/2] += CalcError( b[i], a[i] );
+        err[2+i/2] += CalcError( b[i], a[i+4] );
+    }
+    size_t idx = GetLeastError( err, 4 );
+
+    EncodeAverages( d, a, idx );
+
+    uint terr[2][8] = {};
+    uint8 tsel[16][8];
+    uint8 id[16];
+    for( int i=0; i<16; i++ )
+    {
+        id[i] = (uint8)GetBufId( i, idx );
+    }
+    const uint8* data = src;
+    for( size_t i=0; i<16; i++ )
+    {
+        uint8* sel = tsel[i];
+
+        uint8 c = *data++;
+        uint8 bid = id[i];
+        int32 pix = a[bid] - c;
+
+        for( int t=0; t<8; t++ )
+        {
+            const int32* tab = table[t];
+            uint idx = 0;
+            uint err = std::numeric_limits<uint>::max();
+            for( int j=0; j<4; j++ )
+            {
+                int32 v = tab[j];
+                uint local = sq( v + pix );
+                if( local < err )
+                {
+                    err = local;
+                    idx = j;
+                }
+            }
+            sel[t] = idx;
+            terr[bid%2][t] += err;
+        }
+    }
+    size_t tidx[2];
+    tidx[0] = GetLeastError( terr[0], 8 );
+    tidx[1] = GetLeastError( terr[1], 8 );
+
+    d |= tidx[0] << 2;
+    d |= tidx[1] << 5;
+    for( int i=0; i<16; i++ )
+    {
+        uint64 t = tsel[i][tidx[id[i]%2]];
+        d |= ( t & 0x1 ) << ( i + 32 );
+        d |= ( t & 0x2 ) << ( i + 47 );
+    }
+
+    return d;
 }
 
 void BlockData::ProcessBlocksRGB( const uint8* src, uint64* dst, uint num )
