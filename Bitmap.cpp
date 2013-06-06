454796c39b09d29053ef0cda317a29be020dc000
Bartosz Taudul <wolf.pld@gmail.com>
2013-06-06 02:05:16 +0200

    Support reading raw data.

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -14,74 +14,98 @@ Bitmap::Bitmap( const char* fn )
     FILE* f = fopen( fn, "rb" );
     assert( f );
 
-    unsigned int sig_read = 0;
-    int bit_depth, color_type, interlace_type;
-
-    png_structp png_ptr = png_create_read_struct( PNG_LIBPNG_VER_STRING, NULL, NULL, NULL );
-    png_infop info_ptr = png_create_info_struct( png_ptr );
-    setjmp( png_jmpbuf( png_ptr ) );
-
-    png_init_io( png_ptr, f );
-    png_set_sig_bytes( png_ptr, sig_read );
-
-    png_uint_32 w, h;
-
-    png_read_info( png_ptr, info_ptr );
-    png_get_IHDR( png_ptr, info_ptr, &w, &h, &bit_depth, &color_type, &interlace_type, NULL, NULL );
-
-    m_size = v2i( w, h );
-
-    png_set_strip_16( png_ptr );
-    if( color_type == PNG_COLOR_TYPE_PALETTE )
+    char buf[3];
+    const char raw[3] = { 'r', 'a', 'w' };
+    fread( buf, 1, 3, f );
+    if( memcmp( buf, raw, 3 ) == 0 )
     {
-        png_set_palette_to_rgb( png_ptr );
+        uint8 a;
+        fread( &a, 1, 1, f );
+        m_alpha = a == 1;
+        uint32 d;
+        fread( &d, 1, 4, f );
+        m_size.x = d;
+        fread( &d, 1, 4, f );
+        m_size.y = d;
+        DBGPRINT( "Raw bitmap " << fn << "  " << m_size.x << "x" << m_size.y );
+
+        m_data = new uint32[m_size.x*m_size.y];
+        fread( m_data, 1, m_size.x*m_size.y*4, f );
     }
-    else if( color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8 )
+    else
     {
-        png_set_expand_gray_1_2_4_to_8( png_ptr );
-    }
-    if( png_get_valid( png_ptr, info_ptr, PNG_INFO_tRNS ) )
-    {
-        png_set_tRNS_to_alpha( png_ptr );
-    }
-    if( color_type == PNG_COLOR_TYPE_GRAY_ALPHA )
-    {
-        png_set_gray_to_rgb(png_ptr);
-    }
+        fseek( f, 0, SEEK_SET );
 
-    switch( color_type )
-    {
-    case PNG_COLOR_TYPE_PALETTE:
-        if( !png_get_valid( png_ptr, info_ptr, PNG_INFO_tRNS ) )
+        unsigned int sig_read = 0;
+        int bit_depth, color_type, interlace_type;
+
+        png_structp png_ptr = png_create_read_struct( PNG_LIBPNG_VER_STRING, NULL, NULL, NULL );
+        png_infop info_ptr = png_create_info_struct( png_ptr );
+        setjmp( png_jmpbuf( png_ptr ) );
+
+        png_init_io( png_ptr, f );
+        png_set_sig_bytes( png_ptr, sig_read );
+
+        png_uint_32 w, h;
+
+        png_read_info( png_ptr, info_ptr );
+        png_get_IHDR( png_ptr, info_ptr, &w, &h, &bit_depth, &color_type, &interlace_type, NULL, NULL );
+
+        m_size = v2i( w, h );
+
+        png_set_strip_16( png_ptr );
+        if( color_type == PNG_COLOR_TYPE_PALETTE )
         {
+            png_set_palette_to_rgb( png_ptr );
+        }
+        else if( color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8 )
+        {
+            png_set_expand_gray_1_2_4_to_8( png_ptr );
+        }
+        if( png_get_valid( png_ptr, info_ptr, PNG_INFO_tRNS ) )
+        {
+            png_set_tRNS_to_alpha( png_ptr );
+        }
+        if( color_type == PNG_COLOR_TYPE_GRAY_ALPHA )
+        {
+            png_set_gray_to_rgb(png_ptr);
+        }
+
+        switch( color_type )
+        {
+        case PNG_COLOR_TYPE_PALETTE:
+            if( !png_get_valid( png_ptr, info_ptr, PNG_INFO_tRNS ) )
+            {
+                png_set_filler( png_ptr, 0xff, PNG_FILLER_AFTER );
+                m_alpha = false;
+            }
+            break;
+        case PNG_COLOR_TYPE_GRAY_ALPHA:
+            png_set_gray_to_rgb( png_ptr );
+            break;
+        case PNG_COLOR_TYPE_RGB:
             png_set_filler( png_ptr, 0xff, PNG_FILLER_AFTER );
             m_alpha = false;
+            break;
+        default:
+            break;
         }
-        break;
-    case PNG_COLOR_TYPE_GRAY_ALPHA:
-        png_set_gray_to_rgb( png_ptr );
-        break;
-    case PNG_COLOR_TYPE_RGB:
-        png_set_filler( png_ptr, 0xff, PNG_FILLER_AFTER );
-        m_alpha = false;
-        break;
-    default:
-        break;
+
+        DBGPRINT( "Bitmap " << fn << "  " << w << "x" << h );
+
+        m_data = new uint32[w*h];
+        uint32* ptr = m_data;
+        while( h-- )
+        {
+            png_read_rows( png_ptr, (png_bytepp)&ptr, NULL, 1 );
+            ptr += w;
+        }
+
+        png_read_end( png_ptr, info_ptr );
+
+        png_destroy_read_struct( &png_ptr, &info_ptr, NULL );
     }
 
-    DBGPRINT( "Bitmap " << fn << "  " << w << "x" << h );
-
-    m_data = new uint32[w*h];
-    uint32* ptr = m_data;
-    while( h-- )
-    {
-        png_read_rows( png_ptr, (png_bytepp)&ptr, NULL, 1 );
-        ptr += w;
-    }
-
-    png_read_end( png_ptr, info_ptr );
-
-    png_destroy_read_struct( &png_ptr, &info_ptr, NULL );
     fclose( f );
 }
 
