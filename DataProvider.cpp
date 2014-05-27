1226d3e7c7df00915d883dddc3ac0ec6daa6e9c2
Bartosz Taudul <bartosz.taudul@game-lion.com>
2014-05-27 16:18:29 +0200

    Store mipmap cascade of bitmaps in DataProvider.

--- DataProvider.cpp
+++ DataProvider.cpp
@@ -1,12 +1,16 @@
+#include <assert.h>
+
 #include "DataProvider.hpp"
+#include "MipMap.hpp"
 
 enum { Lines = 32 };
 
 DataProvider::DataProvider( const char* fn, bool mipmap )
-    : m_bmp( new Bitmap( fn, Lines ) )
-    , m_offset( 0 )
+    : m_offset( 0 )
     , m_mipmap( mipmap )
 {
+    m_bmp.emplace_back( new Bitmap( fn, Lines ) );
+    m_current = m_bmp[0].get();
 }
 
 DataProvider::~DataProvider()
@@ -24,13 +28,13 @@ DataPart DataProvider::NextPart()
     bool done;
 
     DataPart ret = {
-        m_bmp->NextBlock( lines, done ),
-        m_bmp->Size().x,
+        m_current->NextBlock( lines, done ),
+        m_current->Size().x,
         lines,
         m_offset
     };
 
-    m_offset += m_bmp->Size().x / 4 * lines;
+    m_offset += m_current->Size().x / 4 * lines;
 
     return ret;
 }
--- DataProvider.hpp
+++ DataProvider.hpp
@@ -2,6 +2,7 @@
 #define __DATAPROVIDER_HPP__
 
 #include <memory>
+#include <vector>
 
 #include "Bitmap.hpp"
 #include "Types.hpp"
@@ -24,12 +25,13 @@ public:
 
     DataPart NextPart();
 
-    bool Alpha() const { return m_bmp->Alpha(); }
-    const v2i& Size() const { return m_bmp->Size(); }
-    const Bitmap& ImageData() const { return *m_bmp; }
+    bool Alpha() const { return m_bmp[0]->Alpha(); }
+    const v2i& Size() const { return m_bmp[0]->Size(); }
+    const Bitmap& ImageData() const { return *m_bmp[0]; }
 
 private:
-    std::unique_ptr<Bitmap> m_bmp;
+    std::vector<std::unique_ptr<Bitmap>> m_bmp;
+    Bitmap* m_current;
     int m_offset;
     bool m_mipmap;
 };
