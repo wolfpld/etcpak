8cd2a99cdc77db15de1f9505916c4820e3aa65f6
Bartosz Taudul <bartosz.taudul@game-lion.com>
2015-02-19 16:54:04 +0100

    Ignore data in two lowest mip levels (2x2, 1x1).

--- BitmapDownsampled.cpp
+++ BitmapDownsampled.cpp
@@ -20,45 +20,65 @@ BitmapDownsampled::BitmapDownsampled( const Bitmap& bmp, uint lines )
     if( m_size.x < w || m_size.y < h )
     {
         memset( m_data, 0, w*h*sizeof( uint32 ) );
-    }
-
-    m_linesLeft = h / 4;
-    m_load = std::async( [this, &bmp, w, h]() mutable
-    {
-        auto ptr = m_data;
-        auto src1 = bmp.Data();
-        auto src2 = src1 + bmp.Size().x;
+        m_linesLeft = h / 4;
         uint lines = 0;
         for( int i=0; i<h/4; i++ )
         {
             for( int j=0; j<4; j++ )
             {
-                for( int k=0; k<m_size.x; k++ )
+                lines++;
+                if( lines > m_lines )
                 {
-                    int r = ( ( *src1 & 0x000000FF ) + ( *(src1+1) & 0x000000FF ) + ( *src2 & 0x000000FF ) + ( *(src2+1) & 0x000000FF ) ) / 4;
-                    int g = ( ( ( *src1 & 0x0000FF00 ) + ( *(src1+1) & 0x0000FF00 ) + ( *src2 & 0x0000FF00 ) + ( *(src2+1) & 0x0000FF00 ) ) / 4 ) & 0x0000FF00;
-                    int b = ( ( ( *src1 & 0x00FF0000 ) + ( *(src1+1) & 0x00FF0000 ) + ( *src2 & 0x00FF0000 ) + ( *(src2+1) & 0x00FF0000 ) ) / 4 ) & 0x00FF0000;
-                    int a = ( ( ( ( ( *src1 & 0xFF000000 ) >> 8 ) + ( ( *(src1+1) & 0xFF000000 ) >> 8 ) + ( ( *src2 & 0xFF000000 ) >> 8 ) + ( ( *(src2+1) & 0xFF000000 ) >> 8 ) ) / 4 ) & 0x00FF0000 ) << 8;
-                    *ptr++ = r | g | b | a;
-                    src1 += 2;
-                    src2 += 2;
+                    lines = 0;
+                    m_sema.unlock();
                 }
-                src1 += m_size.x * 2;
-                src2 += m_size.x * 2;
-            }
-            lines++;
-            if( lines >= m_lines )
-            {
-                lines = 0;
-                m_sema.unlock();
             }
         }
-
         if( lines != 0 )
         {
             m_sema.unlock();
         }
-    } );
+    }
+    else
+    {
+        m_linesLeft = h / 4;
+        m_load = std::async( [this, &bmp, w, h]() mutable
+        {
+            auto ptr = m_data;
+            auto src1 = bmp.Data();
+            auto src2 = src1 + bmp.Size().x;
+            uint lines = 0;
+            for( int i=0; i<h/4; i++ )
+            {
+                for( int j=0; j<4; j++ )
+                {
+                    for( int k=0; k<m_size.x; k++ )
+                    {
+                        int r = ( ( *src1 & 0x000000FF ) + ( *(src1+1) & 0x000000FF ) + ( *src2 & 0x000000FF ) + ( *(src2+1) & 0x000000FF ) ) / 4;
+                        int g = ( ( ( *src1 & 0x0000FF00 ) + ( *(src1+1) & 0x0000FF00 ) + ( *src2 & 0x0000FF00 ) + ( *(src2+1) & 0x0000FF00 ) ) / 4 ) & 0x0000FF00;
+                        int b = ( ( ( *src1 & 0x00FF0000 ) + ( *(src1+1) & 0x00FF0000 ) + ( *src2 & 0x00FF0000 ) + ( *(src2+1) & 0x00FF0000 ) ) / 4 ) & 0x00FF0000;
+                        int a = ( ( ( ( ( *src1 & 0xFF000000 ) >> 8 ) + ( ( *(src1+1) & 0xFF000000 ) >> 8 ) + ( ( *src2 & 0xFF000000 ) >> 8 ) + ( ( *(src2+1) & 0xFF000000 ) >> 8 ) ) / 4 ) & 0x00FF0000 ) << 8;
+                        *ptr++ = r | g | b | a;
+                        src1 += 2;
+                        src2 += 2;
+                    }
+                    src1 += m_size.x * 2;
+                    src2 += m_size.x * 2;
+                }
+                lines++;
+                if( lines >= m_lines )
+                {
+                    lines = 0;
+                    m_sema.unlock();
+                }
+            }
+
+            if( lines != 0 )
+            {
+                m_sema.unlock();
+            }
+        } );
+    }
 }
 
 BitmapDownsampled::~BitmapDownsampled()
