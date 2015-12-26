9edf9199a25825f2fdf3c9ac23c9eef2385380cf
Daniel Jungmann <daniel@arkaos.net>
2015-12-27 00:35:52 +0100

    Big avx2 rewrite and small general improvements All functions used to compress an etc1 block are now converted to avx2. The selector indices are directly bitpacked (could be tried for the SSE4.1 version too). Calculation of square sum for blocks is not needed, since every partition scheme in a block has the same square sum. Replaced it with big constant to prevent negative values.

--- ProcessRGB_AVX2.cpp
+++ ProcessRGB_AVX2.cpp
@@ -24,184 +24,137 @@ namespace
 
 typedef std::array<uint16, 4> v4i;
 
-void Average_AVX2( const uint8* data, v4i* a )
+__m256i Sum4_AVX2( const uint8* data)
 {
     __m128i d0 = _mm_loadu_si128(((__m128i*)data) + 0);
     __m128i d1 = _mm_loadu_si128(((__m128i*)data) + 1);
     __m128i d2 = _mm_loadu_si128(((__m128i*)data) + 2);
     __m128i d3 = _mm_loadu_si128(((__m128i*)data) + 3);
 
-    __m256i t0 = _mm256_cvtepu8_epi16(d0);
-    __m256i t1 = _mm256_cvtepu8_epi16(d1);
-    __m256i t2 = _mm256_cvtepu8_epi16(d2);
-    __m256i t3 = _mm256_cvtepu8_epi16(d3);
-
-    __m256i sum0 = _mm256_add_epi16(t0, t1);
-    __m256i sum1 = _mm256_add_epi16(t2, t3);
-
-    __m256i sum0l = _mm256_unpacklo_epi16(sum0, _mm256_setzero_si256());
-    __m256i sum0h = _mm256_unpackhi_epi16(sum0, _mm256_setzero_si256());
-    __m256i sum1l = _mm256_unpacklo_epi16(sum1, _mm256_setzero_si256());
-    __m256i sum1h = _mm256_unpackhi_epi16(sum1, _mm256_setzero_si256());
-
-    __m256i b0 = _mm256_add_epi32(sum0l, sum0h);
-    __m256i b1 = _mm256_add_epi32(sum1l, sum1h);
-
-    __m256i t4 = _mm256_add_epi32(b0, b1);
-    __m256i t5 = _mm256_permute2x128_si256(t4, t4, (1) | (0 << 4));
-    __m256i t6 = _mm256_permute2x128_si256(b0, b1, (2) | (0 << 4));
-    __m256i t7 = _mm256_permute2x128_si256(b0, b1, (3) | (1 << 4));
-    __m256i t8 = _mm256_add_epi32(t6, t7);
-
-    __m256i v0 = _mm256_add_epi32(t5, _mm256_set1_epi32(4));
-    __m256i v1 = _mm256_add_epi32(t8, _mm256_set1_epi32(4));
-
-    __m256i a0 = _mm256_shuffle_epi32(_mm256_srli_epi32(v0, 3), _MM_SHUFFLE(3, 0, 1, 2));
-    __m256i a1 = _mm256_shuffle_epi32(_mm256_srli_epi32(v1, 3), _MM_SHUFFLE(3, 0, 1, 2));
-
-    __m256i a2 = _mm256_packus_epi32(a0, a1);
-    __m256i a3 = _mm256_permute4x64_epi64(a2, _MM_SHUFFLE(2, 0, 3, 1));
-
-    _mm256_storeu_si256((__m256i*)&a[0], a3);
-}
-
-void CalcErrorBlock_AVX2( const uint8* data, uint err[4][4] )
-{
-    __m128i d0 = _mm_loadu_si128(((__m128i*)data) + 0);
-    __m128i d1 = _mm_loadu_si128(((__m128i*)data) + 1);
-    __m128i d2 = _mm_loadu_si128(((__m128i*)data) + 2);
-    __m128i d3 = _mm_loadu_si128(((__m128i*)data) + 3);
-
-    __m128i dm0 = _mm_and_si128(d0, _mm_setr_epi8(-1, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1, 0));
-    __m128i dm1 = _mm_and_si128(d1, _mm_setr_epi8(-1, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1, 0));
-    __m128i dm2 = _mm_and_si128(d2, _mm_setr_epi8(-1, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1, 0));
-    __m128i dm3 = _mm_and_si128(d3, _mm_setr_epi8(-1, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1, 0, -1, -1, -1, 0));
+    __m128i dm0 = _mm_and_si128(d0, _mm_set1_epi32(0x00FFFFFF));
+    __m128i dm1 = _mm_and_si128(d1, _mm_set1_epi32(0x00FFFFFF));
+    __m128i dm2 = _mm_and_si128(d2, _mm_set1_epi32(0x00FFFFFF));
+    __m128i dm3 = _mm_and_si128(d3, _mm_set1_epi32(0x00FFFFFF));
 
     __m256i t0 = _mm256_cvtepu8_epi16(dm0);
     __m256i t1 = _mm256_cvtepu8_epi16(dm1);
     __m256i t2 = _mm256_cvtepu8_epi16(dm2);
     __m256i t3 = _mm256_cvtepu8_epi16(dm3);
 
-    __m256i sqrSum0, sqrSum1;
-
-    {
-        __m256i sum0 = _mm256_madd_epi16(t0, t0);
-        __m256i sum1 = _mm256_madd_epi16(t1, t1);
-        __m256i sum2 = _mm256_madd_epi16(t2, t2);
-        __m256i sum3 = _mm256_madd_epi16(t3, t3);
-
-        __m256i sum4 = _mm256_add_epi32(sum0, sum1);
-        __m256i sum5 = _mm256_add_epi32(sum2, sum3);
-
-        __m256i sum6 = _mm256_hadd_epi32(sum4, sum5);
-
-        __m256i sum7 = _mm256_hadd_epi32(sum6, sum6);
-
-        __m256i sum8 = _mm256_permute4x64_epi64(sum7, _MM_SHUFFLE(3, 1, 2, 0));
-
-        __m256i t = _mm256_shuffle_epi32(sum8, _MM_SHUFFLE(2, 0, 3, 1));
-
-        __m256i sum = _mm256_add_epi32(t, sum8);
-
-        sqrSum0 = _mm256_permutevar8x32_epi32(sum, _mm256_setr_epi32(3, 3, 3, 3, 0, 0, 0, 0));
-        sqrSum1 = _mm256_permutevar8x32_epi32(sum, _mm256_setr_epi32(1, 1, 1, 1, 2, 2, 2, 2));
-
-        sqrSum0 = _mm256_and_si256(sqrSum0, _mm256_setr_epi32(0, 0, 0, -1, 0, 0, 0, -1));
-        sqrSum1 = _mm256_and_si256(sqrSum1, _mm256_setr_epi32(0, 0, 0, -1, 0, 0, 0, -1));
-    }
-
     __m256i sum0 = _mm256_add_epi16(t0, t1);
     __m256i sum1 = _mm256_add_epi16(t2, t3);
 
-    __m256i sum0l = _mm256_unpacklo_epi16(sum0, _mm256_setzero_si256());
-    __m256i sum0h = _mm256_unpackhi_epi16(sum0, _mm256_setzero_si256());
-    __m256i sum1l = _mm256_unpacklo_epi16(sum1, _mm256_setzero_si256());
-    __m256i sum1h = _mm256_unpackhi_epi16(sum1, _mm256_setzero_si256());
+    __m256i s0 = _mm256_permute2x128_si256(sum0, sum1, (0) | (3 << 4)); // 0, 0, 3, 3
+    __m256i s1 = _mm256_permute2x128_si256(sum0, sum1, (1) | (2 << 4)); // 1, 1, 2, 2
 
-    __m256i b0 = _mm256_add_epi32(sum0l, sum0h);
-    __m256i b1 = _mm256_add_epi32(sum1l, sum1h);
+    __m256i s2 = _mm256_permute4x64_epi64(s0, _MM_SHUFFLE(1, 3, 0, 2));
+    __m256i s3 = _mm256_permute4x64_epi64(s0, _MM_SHUFFLE(0, 2, 1, 3));
+    __m256i s4 = _mm256_permute4x64_epi64(s1, _MM_SHUFFLE(3, 1, 0, 2));
+    __m256i s5 = _mm256_permute4x64_epi64(s1, _MM_SHUFFLE(2, 0, 1, 3));
 
-    __m256i t4 = _mm256_add_epi32(b0, b1);
-    __m256i t5 = _mm256_permute2x128_si256(t4, t4, (1) | (0 << 4));
-    __m256i t6 = _mm256_permute2x128_si256(b0, b1, (2) | (0 << 4));
-    __m256i t7 = _mm256_permute2x128_si256(b0, b1, (3) | (1 << 4));
-    __m256i t8 = _mm256_add_epi32(t6, t7);
-
-    __m256i a0 = _mm256_or_si256(t5, sqrSum0);
-    __m256i a1 = _mm256_or_si256(t8, sqrSum1);
-
-    _mm256_storeu_si256((__m256i*)&err[0], a1);
-    _mm256_storeu_si256((__m256i*)&err[2], a0);
+    __m256i sum5 = _mm256_add_epi16(s2, s3); //   3,   0,   3,   0
+    __m256i sum6 = _mm256_add_epi16(s4, s5); //   2,   1,   1,   2
+    return _mm256_add_epi16(sum5, sum6);     // 3+2, 0+1, 3+1, 3+2
 }
 
-uint CalcError( const uint block[4], const v4i& average )
+__m256i Average_AVX2( const __m256i data)
 {
-    uint err = block[3];
-    err -= block[0] * 2 * average[2];
-    err -= block[1] * 2 * average[1];
-    err -= block[2] * 2 * average[0];
-    err += 8 * ( sq( average[0] ) + sq( average[1] ) + sq( average[2] ) );
-    return err;
+    __m256i a = _mm256_add_epi16(data, _mm256_set1_epi16(4));
+
+    return _mm256_srli_epi16(a, 3);
 }
 
-void ProcessAverages_AVX2( v4i* a )
+__m128i CalcErrorBlock_AVX2( const __m256i data, const v4i* a)
 {
-    {
-        __m256i d = _mm256_loadu_si256((__m256i*)a[0].data());
+    //
+    __m256i a0 = _mm256_load_si256((__m256i*)a[0].data());
+    __m256i a1 = _mm256_load_si256((__m256i*)a[4].data());
+    __m256i a2 = _mm256_slli_epi16(a0, 1);
+    __m256i a3 = _mm256_slli_epi16(a1, 1);
 
-        __m256i t = _mm256_add_epi16(_mm256_mullo_epi16(d, _mm256_set1_epi16(31)), _mm256_set1_epi16(128));
+    // err = 8 * ( sq( average[0] ) + sq( average[1] ) + sq( average[2] ) );
+    __m256i a4 = _mm256_madd_epi16(a0, a0);
+    __m256i a5 = _mm256_madd_epi16(a1, a1);
 
-        __m256i c = _mm256_srli_epi16(_mm256_add_epi16(t, _mm256_srli_epi16(t, 8)), 8);
+    __m256i a6 = _mm256_hadd_epi32(a4, a5);
+    __m256i a7 = _mm256_slli_epi32(a6, 3);
 
-        __m256i c1 = _mm256_shuffle_epi32(c, _MM_SHUFFLE(3, 2, 3, 2));
-        __m256i diff = _mm256_sub_epi16(c, c1);
-        diff = _mm256_max_epi16(diff, _mm256_set1_epi16(-4));
-        diff = _mm256_min_epi16(diff, _mm256_set1_epi16(3));
+    __m256i a8 = _mm256_add_epi32(a7, _mm256_set1_epi32(0x3FFFFFFF)); // Big value to prevent negative values, but small enough to prevent overflow
 
-        __m256i co = _mm256_add_epi16(c1, diff);
+    // average is not swapped
+    // err -= block[0] * 2 * average[0];
+    // err -= block[1] * 2 * average[1];
+    // err -= block[2] * 2 * average[2];
+    __m256i b0 = _mm256_madd_epi16(a2, data);
+    __m256i b1 = _mm256_madd_epi16(a3, data);
 
-        c = _mm256_blend_epi16(co, c, 0xF0);
+    __m256i b2 = _mm256_hadd_epi32(b0, b1);
+    __m256i b3 = _mm256_sub_epi32(a8, b2);
+    __m256i b4 = _mm256_hadd_epi32(b3, b3);
 
-        __m256i a0 = _mm256_or_si256(_mm256_slli_epi16(c, 3), _mm256_srli_epi16(c, 2));
+    __m256i b5 = _mm256_permutevar8x32_epi32(b4, _mm256_set_epi32(0, 0, 0, 0, 5, 1, 4, 0));
 
-        _mm256_storeu_si256((__m256i*)a[4].data(), a0);
-    }
-
-    {
-        __m256i d = _mm256_loadu_si256((__m256i*)a[0].data());
-
-        __m256i t0 = _mm256_add_epi16(_mm256_mullo_epi16(d, _mm256_set1_epi16(15)), _mm256_set1_epi16(128));
-        __m256i t1 = _mm256_srli_epi16(_mm256_add_epi16(t0, _mm256_srli_epi16(t0, 8)), 8);
-
-        __m256i t2 = _mm256_or_si256(t1, _mm256_slli_epi16(t1, 4));
-
-        _mm256_storeu_si256((__m256i*)a[0].data(), t2);
-    }
+    return _mm256_castsi256_si128(b5);
 }
 
-uint64 EncodeAverages( uint64 d, const v4i* a, size_t idx )
+void ProcessAverages_AVX2(const __m256i d, v4i* a )
 {
-    d |= ( idx << 24 );
+    __m256i t = _mm256_add_epi16(_mm256_mullo_epi16(d, _mm256_set1_epi16(31)), _mm256_set1_epi16(128));
+
+    __m256i c = _mm256_srli_epi16(_mm256_add_epi16(t, _mm256_srli_epi16(t, 8)), 8);
+
+    __m256i c1 = _mm256_shuffle_epi32(c, _MM_SHUFFLE(3, 2, 3, 2));
+    __m256i diff = _mm256_sub_epi16(c, c1);
+    diff = _mm256_max_epi16(diff, _mm256_set1_epi16(-4));
+    diff = _mm256_min_epi16(diff, _mm256_set1_epi16(3));
+
+    __m256i co = _mm256_add_epi16(c1, diff);
+
+    c = _mm256_blend_epi16(co, c, 0xF0);
+
+    __m256i a0 = _mm256_or_si256(_mm256_slli_epi16(c, 3), _mm256_srli_epi16(c, 2));
+
+    _mm256_store_si256((__m256i*)a[4].data(), a0);
+
+    __m256i t0 = _mm256_add_epi16(_mm256_mullo_epi16(d, _mm256_set1_epi16(15)), _mm256_set1_epi16(128));
+    __m256i t1 = _mm256_srli_epi16(_mm256_add_epi16(t0, _mm256_srli_epi16(t0, 8)), 8);
+
+    __m256i t2 = _mm256_or_si256(t1, _mm256_slli_epi16(t1, 4));
+
+    _mm256_store_si256((__m256i*)a[0].data(), t2);
+}
+
+uint64 EncodeAverages_AVX2( const v4i* a, size_t idx )
+{
+    uint64 d = ( idx << 24 );
     size_t base = idx << 1;
+
+    __m128i a0 = _mm_load_si128((const __m128i*)a[base].data());
+
+    __m128i r0, r1;
 
     if( ( idx & 0x2 ) == 0 )
     {
-        for( int i=0; i<3; i++ )
-        {
-            d |= uint64( a[base+0][i] >> 4 ) << ( i*8 );
-            d |= uint64( a[base+1][i] >> 4 ) << ( i*8 + 4 );
-        }
+        r0 = _mm_srli_epi16(a0, 4);
+
+        __m128i a1 = _mm_unpackhi_epi64(r0, r0);
+        r1 = _mm_slli_epi16(a1, 4);
     }
     else
     {
-        for( int i=0; i<3; i++ )
-        {
-            d |= uint64( a[base+1][i] & 0xF8 ) << ( i*8 );
-            int32 c = ( ( a[base+0][i] & 0xF8 ) - ( a[base+1][i] & 0xF8 ) ) >> 3;
-            c &= ~0xFFFFFFF8;
-            d |= ((uint64)c) << ( i*8 );
-        }
+        __m128i a1 = _mm_and_si128(a0, _mm_set1_epi16(0xFFF8));
+
+        r0 = _mm_unpackhi_epi64(a1, a1);
+        __m128i a2 = _mm_sub_epi16(a1, r0);
+        __m128i a3 = _mm_srai_epi16(a2, 3);
+        r1 = _mm_and_si128(a3, _mm_set1_epi16(0x07));
     }
+
+    __m128i r2 = _mm_or_si128(r0, r1);
+    // do missing swap for average values
+    __m128i r3 = _mm_shufflelo_epi16(r2, _MM_SHUFFLE(3, 0, 1, 2));
+    __m128i r4 = _mm_packus_epi16(r3, _mm_setzero_si128());
+    d |= _mm_cvtsi128_si32(r4);
 
     return d;
 }
@@ -229,19 +182,13 @@ uint64 CheckSolid_AVX2( const uint8* src )
         ( uint( src[2] & 0xF8 ) );
 }
 
-void PrepareAverages_AVX2( v4i a[8], const uint8* src, uint err[4] )
+__m128i PrepareAverages_AVX2( v4i a[8], const uint8* src)
 {
-    Average_AVX2( src, a );
-    ProcessAverages_AVX2( a );
+    __m256i sum4 = Sum4_AVX2( src );
 
-    uint errblock[4][4];
-    CalcErrorBlock_AVX2( src, errblock );
+    ProcessAverages_AVX2(Average_AVX2( sum4 ), a );
 
-    for( int i=0; i<4; i++ )
-    {
-        err[i/2] += CalcError( errblock[i], a[i] );
-        err[2+i/2] += CalcError( errblock[i], a[i+4] );
-    }
+    return CalcErrorBlock_AVX2( sum4, a);
 }
 
 void FindBestFit_AVX2( uint32 terr[2][8], uint32 tsel[8], v4i a[8], const uint32* id, const uint8* data)
@@ -254,11 +201,10 @@ void FindBestFit_AVX2( uint32 terr[2][8], uint32 tsel[8], v4i a[8], const uint32
         uint32* ter = terr[bid%2];
 
         __m128i a0 = _mm_loadl_epi64((const __m128i*)a[bid].data());
-        __m128i a1 = _mm_shufflelo_epi16(a0, _MM_SHUFFLE(3, 0, 1, 2));
-        __m128i rgb = _mm_loadl_epi64((const __m128i*)data);
+        __m128i rgb = _mm_loadl_epi64((const __m128i*)(data + i * 4));
 
         __m128i rgb16 = _mm_cvtepu8_epi16(rgb);
-        __m128i d = _mm_sub_epi16(_mm_broadcastq_epi64(a1), rgb16);
+        __m128i d = _mm_sub_epi16(_mm_broadcastq_epi64(a0), rgb16);
 
         // The scaling values are divided by two and rounded, to allow the differences to be in the range of signed int16
         // This produces slightly different results, but is significant faster
@@ -269,8 +215,6 @@ void FindBestFit_AVX2( uint32 terr[2][8], uint32 tsel[8], v4i a[8], const uint32
         __m128i pix0 = _mm_broadcastw_epi16(pixel2);
         __m128i pix1 = _mm_broadcastw_epi16(_mm_srli_epi32(pixel2, 16));
         __m256i pixel = _mm256_insertf128_si256(_mm256_castsi128_si256(pix0), pix1, 1);
-
-        data += 8;
 
         __m256i pix = _mm256_abs_epi16(pixel);
 
@@ -293,10 +237,11 @@ void FindBestFit_AVX2( uint32 terr[2][8], uint32 tsel[8], v4i a[8], const uint32
         // Squaring the minimum error to produce correct values when adding
         __m256i squareErrorSum = _mm256_madd_epi16(minError2, minError2);
 
-        __m256i squareError = _mm256_add_epi32(squareErrorSum, _mm256_loadu_si256((__m256i*)ter));
+        __m256i squareError = _mm256_add_epi32(squareErrorSum, _mm256_load_si256((__m256i*)ter));
 
-        _mm256_storeu_si256((__m256i*)ter, squareError);
+        _mm256_store_si256((__m256i*)ter, squareError);
 
+        // Packing selector bits
         __m256i minIndexLo0 = _mm256_unpacklo_epi16(minIndex0, minIndex1);
         __m256i minIndexHi0 = _mm256_unpackhi_epi16(minIndex0, minIndex1);
 
@@ -304,42 +249,40 @@ void FindBestFit_AVX2( uint32 terr[2][8], uint32 tsel[8], v4i a[8], const uint32
         __m256i minIndexHi1 = _mm256_permute2x128_si256(minIndexLo0, minIndexHi0, (1) | (3 << 4));
 
         __m256i minIndexLo2 = _mm256_sll_epi32(minIndexLo1, _mm_set1_epi64x(i));
-        __m256i minIndexHi2 = _mm256_sll_epi32(minIndexHi1, _mm_set1_epi64x(i + 1));
+        __m256i minIndexHi2 = _mm256_sll_epi32(minIndexHi1, _mm_set1_epi64x(i));
+        __m256i minIndexHi3 = _mm256_add_epi32(minIndexHi2, minIndexHi2);
 
         sel = _mm256_or_si256(sel, minIndexLo2);
-        sel = _mm256_or_si256(sel, minIndexHi2);
+        sel = _mm256_or_si256(sel, minIndexHi3);
     }
 
-    _mm256_storeu_si256((__m256i*)tsel, sel);
+    _mm256_store_si256((__m256i*)tsel, sel);
 }
 
 uint64 EncodeSelectors_AVX2( uint64 d, const uint32 terr[2][8], const uint32 tsel[8], const bool rotate)
 {
     size_t tidx[2];
-    __m256i err0 = _mm256_loadu_si256((const __m256i*)terr[0]);
-    __m256i err1 = _mm256_loadu_si256((const __m256i*)terr[1]);
+
+    // Get index of minimum error (terr[0] and terr[1])
+    __m256i err0 = _mm256_load_si256((const __m256i*)terr[0]);
+    __m256i err1 = _mm256_load_si256((const __m256i*)terr[1]);
 
     __m256i errLo = _mm256_permute2x128_si256(err0, err1, (0) | (2 << 4));
     __m256i errHi = _mm256_permute2x128_si256(err0, err1, (1) | (3 << 4));
 
     __m256i errMin0 = _mm256_min_epu32(errLo, errHi);
 
-    __m256i errMin1 = _mm256_shuffle_epi32(errMin0, _MM_SHUFFLE(1, 1, 1, 1));
-    __m256i errMin2 = _mm256_shuffle_epi32(errMin0, _MM_SHUFFLE(2, 2, 2, 2));
-    __m256i errMin3 = _mm256_shuffle_epi32(errMin0, _MM_SHUFFLE(3, 3, 3, 3));
+    __m256i errMin1 = _mm256_shuffle_epi32(errMin0, _MM_SHUFFLE(2, 3, 0, 1));
+    __m256i errMin2 = _mm256_min_epu32(errMin0, errMin1);
 
-    __m256i errMin4 = _mm256_min_epu32(errMin0, errMin1);
-    __m256i errMin5 = _mm256_min_epu32(errMin2, errMin3);
+    __m256i errMin3 = _mm256_shuffle_epi32(errMin2, _MM_SHUFFLE(1, 0, 3, 2));
+    __m256i errMin4 = _mm256_min_epu32(errMin3, errMin2);
 
-    __m256i errMin6 = _mm256_min_epu32(errMin4, errMin5);
+    __m256i errMin5 = _mm256_permute2x128_si256(errMin4, errMin4, (0) | (0 << 4));
+    __m256i errMin6 = _mm256_permute2x128_si256(errMin4, errMin4, (1) | (1 << 4));
 
-    __m256i errMin7 = _mm256_shuffle_epi32(errMin6, _MM_SHUFFLE(0, 0, 0, 0));
-
-    __m256i errMin8 = _mm256_permute2x128_si256(errMin7, errMin7, (0) | (0 << 4));
-    __m256i errMin9 = _mm256_permute2x128_si256(errMin7, errMin7, (1) | (1 << 4));
-
-    __m256i errMask0 = _mm256_cmpeq_epi32(errMin8, err0);
-    __m256i errMask1 = _mm256_cmpeq_epi32(errMin9, err1);
+    __m256i errMask0 = _mm256_cmpeq_epi32(errMin5, err0);
+    __m256i errMask1 = _mm256_cmpeq_epi32(errMin6, err1);
 
     uint32 mask0 = _mm256_movemask_epi8(errMask0);
     uint32 mask1 = _mm256_movemask_epi8(errMask1);
@@ -364,7 +307,7 @@ uint64 EncodeSelectors_AVX2( uint64 d, const uint32 terr[2][8], const uint32 tse
         t1 &= 0x33333333;
     }
 
-    return d | static_cast<uint64>(t0 | t1) << 32;
+    return d | static_cast<uint64>(_bswap(t0 | t1)) << 32;
 }
 
 }
@@ -374,18 +317,31 @@ uint64 ProcessRGB_AVX2( const uint8* src )
     uint64 d = CheckSolid_AVX2( src );
     if( d != 0 ) return d;
 
-    v4i a[8];
-    uint err[4] = {};
-    PrepareAverages_AVX2( a, src, err );
-    size_t idx = GetLeastError( err, 4 );
-    d = EncodeAverages( d, a, idx );
+    alignas(32) v4i a[8];
 
-    uint32 terr[2][8] = {};
-    uint32 tsel[8];
+    __m128i err0 = PrepareAverages_AVX2( a, src );
+
+    // Get index of minimum error (err0)
+    __m128i err1 = _mm_shuffle_epi32(err0, _MM_SHUFFLE(2, 3, 0, 1));
+    __m128i errMin0 = _mm_min_epu32(err0, err1);
+
+    __m128i errMin1 = _mm_shuffle_epi32(errMin0, _MM_SHUFFLE(1, 0, 3, 2));
+    __m128i errMin2 = _mm_min_epu32(errMin1, errMin0);
+
+    __m128i errMask = _mm_cmpeq_epi32(errMin2, err0);
+
+    uint32 mask = _mm_movemask_epi8(errMask);
+
+    size_t idx = _bit_scan_forward(mask) >> 2;
+
+    d = EncodeAverages_AVX2( a, idx );
+
+    alignas(32) uint32 terr[2][8] = {};
+    alignas(32) uint32 tsel[8];
     auto id = g_id[idx];
     FindBestFit_AVX2( terr, tsel, a, id, src );
 
-    return FixByteOrder( EncodeSelectors_AVX2( d, terr, tsel, (idx % 2) == 1 ) );
+    return EncodeSelectors_AVX2( d, terr, tsel, (idx % 2) == 1 );
 }
 
 #pragma GCC pop_options
