bfb8ee7488791b2cc6ae35f89d28be56c19b067f
Bartosz Taudul <wolf.pld@gmail.com>
2019-06-13 01:36:05 +0200

    Add NEON path to ETC1 compression.
    Based on https://github.com/metora/etcpak/commit/92be152204f72a6673ed8ca882375449d3b0e8aa.
    
    reference: 652.270 ms
    average: 651.385 ms
    calcerrorblock: 643.059 ms
    processaverages: 636.143 ms
    checksolid: 635.358 ms
    findbestfit: 280.859 ms

--- Tables.hpp
+++ Tables.hpp
@@ -6,6 +6,9 @@
 #ifdef __SSE4_1__
 #include <smmintrin.h>
 #endif
+#ifdef __ARM_NEON
+#include <arm_neon.h>
+#endif
 
 extern const int32_t g_table[8][4];
 extern const int64_t g_table256[8][4];
@@ -27,6 +30,10 @@ extern const __m128i g_table256_SIMD[4];
 
 extern const __m128i g_alpha_SIMD[16];
 extern const __m128i g_alphaRange_SIMD;
+#endif
+
+#ifdef __ARM_NEON
+extern const int32x4_t g_table256_NEON[4];
 #endif
 
 #endif
