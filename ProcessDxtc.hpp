9f26b6598301fce6fae9b11fb84f8c3b13902dae
Bartosz Taudul <wolf.pld@gmail.com>
2020-04-30 17:19:32 +0200

    Add missing include.

--- ProcessDxtc.hpp
+++ ProcessDxtc.hpp
@@ -1,6 +1,7 @@
 #ifndef __PROCESSDXT1_HPP__
 #define __PROCESSDXT1_HPP__
 
+#include <stddef.h>
 #include <stdint.h>
 
 void CompressDxt1( const uint32_t* src, uint64_t* dst, uint32_t blocks, size_t width );
